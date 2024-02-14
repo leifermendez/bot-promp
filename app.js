511c01b0ff1b6619f8761e37b83a346dcc23e492
Leifer Mendez <leifer33@gmail.com>
2024-02-14 09:50:01 +0100

    funciona

--- app.js
+++ app.js
@@ -1,40 +1,21 @@
 require("dotenv").config();
-const { createBot, createProvider, createFlow } = require("@bot-whatsapp/bot");
+const { createProvider } = require("@bot-whatsapp/bot");
 const QRPortalWeb = require("@bot-whatsapp/portal");
 const BaileysProvider = require("@bot-whatsapp/provider/baileys");
 const MockAdapter = require("@bot-whatsapp/database/mock");
+const ChatGPTClass = require('./chatgpt.class')
 
-/**
- * ChatGPT
- */
-const ChatGPTClass = require("./chatgpt.class");
-const chatGPT = new ChatGPTClass();
 
-/**
- * Flows
- */
-const flowPrincipal = require("./flows/flowPrincipal");
-const flowAgente = require("./flows/flowAgente");
-const { flowReparacion } = require("./flows/flowReparacion");
-const { flowOfertas } = require("./flows/flowOfertas");
+const createBotChatGpt = async ({ provider, database }) => {
+  return new ChatGPTClass(database, provider)
+}
 
-/**
- * Funcion principal
- */
 const main = async () => {
   const adapterDB = new MockAdapter();
 
-  const adapterFlow = createFlow([
-    flowPrincipal,
-    flowAgente,
-    flowReparacion(chatGPT),
-    flowOfertas(chatGPT),
-  ]);
-  
   const adapterProvider = createProvider(BaileysProvider);
 
-  createBot({
-    flow: adapterFlow,
+  createBotChatGpt({
     provider: adapterProvider,
     database: adapterDB,
   });
--- chatgpt.class.js
+++ chatgpt.class.js
@@ -1,11 +1,14 @@
-require('dotenv').config()
+require('dotenv').config();
+const { CoreClass } = require('@bot-whatsapp/bot');
 
-class ChatGPTClass {
-  queue = []; 
-  optionsGPT = { model: "gpt-3.5-turbo-0301" };
+
+class ChatGPTClass extends CoreClass {
+  queue = [];
+  optionsGPT = { model: "gpt-3.5-turbo" };
   openai = undefined;
 
-  constructor() {
+  constructor(_database, _provider) {
+    super(null, _database, _provider)
     this.init().then();
   }
 
@@ -15,30 +18,45 @@ class ChatGPTClass {
   init = async () => {
     const { ChatGPTAPI } = await import("chatgpt");
     this.openai = new ChatGPTAPI(
-        {
-            apiKey: process.env.OPENAI_API_KEY
-        }
+      {
+        apiKey: process.env.OPENAI_API_KEY
+      }
     );
   };
 
-  /**
-   * Manejador de los mensajes
-   * sun funcion es enviar un mensaje a wahtsapp
-   * @param {*} ctx 
-   */
-  handleMsgChatGPT = async (body) => {
-    const interaccionChatGPT = await this.openai.sendMessage(body, {
-      conversationId: !this.queue.length
-        ? undefined
-        : this.queue[this.queue.length - 1].conversationId,
-      parentMessageId: !this.queue.length
-        ? undefined
-        : this.queue[this.queue.length - 1].id,
-    });
+  handleMsg = async (ctx) => {
 
-    this.queue.push(interaccionChatGPT);
-    return interaccionChatGPT
-  };
+    const state = {
+      getMyState: this.stateHandler.getMyState(ctx.from),
+      get: this.stateHandler.get(ctx.from),
+      getAllState: this.stateHandler.getAllState,
+      update: this.stateHandler.updateState(ctx),
+      clear: this.stateHandler.clear(ctx.from),
+    }
+
+    const { from, body } = ctx
+
+    const conversationList = state.getMyState()?.conversationList ?? []
+    const interaccionChatGPT = await this.openai.sendMessage(body,
+      {
+        conversationId: !conversationList.length
+          ? undefined
+          : conversationList[conversationList.length - 1].conversationId,
+        parentMessageId: !conversationList.length
+          ? undefined
+          : conversationList[conversationList.length - 1].id,
+      });
+
+    conversationList.push(interaccionChatGPT);
+    const parseMessage = {
+      ...interaccionChatGPT,
+      answer: interaccionChatGPT.text
+    }
+
+    await state.update({ threadId: null, conversationList })
+
+    this.sendFlowSimple([parseMessage], from)
+  }
 }
 
 module.exports = ChatGPTClass;
